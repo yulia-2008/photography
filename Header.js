b223ec95e477978c52af90de0ff33fb586c0c719
yulia <juliana.ny2008@gmail.com>
2021-04-05 17:17:00 -0400

    css for header

--- Header.js
+++ Header.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import {View, Text, StyleSheet} from "react-native";
 import {MaterialIcons} from '@expo/vector-icons';
+import { RawButton } from 'react-native-gesture-handler';
 
 
 export default function Header({navigation, title}) {
@@ -10,10 +11,28 @@ export default function Header({navigation, title}) {
     const openMenu = () => {navigation.openDrawer()}
 
     return( 
-        <View>
+        <View style={styles.header}>
             {/* icon for the menu */}
-            <MaterialIcons name="menu" size={28} onPress={openMenu}/>
-            <Text>{title}</Text>
+            <MaterialIcons name="menu" size={28} onPress={openMenu} style={styles.icon}/>
+            <Text style={styles.headerText}>{title}</Text>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    header: {
+        height: "100%",
+        flexDirection: "row",
+        alignItems: "center", 
+        justifyContent: "center",
+    },
+    headerText: {
+        fontSize: 20,
+        fontWeight: "bold",
+    },
+    icon:{
+        position: "absolute",
+        left: 15,
+    }
+
+})
