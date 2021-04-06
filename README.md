1c0be9370071f18a6c0839b4c3a9511d947ba960
yulia <juliana.ny2008@gmail.com>
2021-04-06 15:42:39 -0400

    installing Image Picker, changing the permission in app.json, created select Image method

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 Learning Stack Navigation, Drawer Navigation.
+Added "permissions": ["READ_EXTERNAL_STORAGE", "CAMERA"] to app.json for creating image picker component
--- screens/PhotoUploader.js
+++ screens/PhotoUploader.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import {View, Button, Text, TextInput, StyleSheet} from "react-native";
+import {View, Button, Text, TextInput, TouchableOpacity, StyleSheet} from "react-native";
+import ImagePicker from 'react-native-image-picker';
 
 export default function PhotoUploader() {
 
@@ -8,6 +9,48 @@ export default function PhotoUploader() {
     const changeHandler = newText => {
         setText(newText)
       }
+
+    const selectImage = () => {
+        let options = {
+            title: 'Select Image',
+            // maxWidth: 256,
+            // maxHeight: 256,
+            // storageOptions: {
+            //     skipBackup: true
+            // }
+        //     customButtons: [
+        //     { 
+        //       name: 'customOptionKey', 
+        //       title: 'Choose file from Custom Option' 
+        //     },
+        //   ],
+          storageOptions: {
+            skipBackup: true,
+            path: 'images',
+          },
+        };
+    
+        ImagePicker.showImagePicker(options, response => {
+          console.log('Response = ', response);
+    
+          if (res.didCancel) {
+            console.log('User cancelled photo picker');
+            Alert.alert('You did not select any image'); 
+          }     
+           else if (res.error) {
+            console.log('ImagePicker Error: ', response.error);
+          } 
+          else if (res.customButton) {
+            console.log('User tapped custom button: ', res.customButton);
+          } 
+          else {
+            let source = res;
+            this.setState({
+              resourcePath: source,
+            });
+          }
+        })
+    };  
 
     return (
          <View style={styles.container}>
@@ -24,6 +67,11 @@ export default function PhotoUploader() {
                 keyboardType="text"
                 required
                 multiline={true} />
+
+            <TouchableOpacity onPress={selectImage}> 
+                <Text>Pick an image</Text>
+            </TouchableOpacity>
+
             <Button title="Submit"></Button>
                     {/* button can have color prop only, styles dont work with button component, 
                         need to create custom buttom component*/}
