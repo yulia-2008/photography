b34c958a180da36d0340e3184ba59f3cffad416c
yulia <juliana.ny2008@gmail.com>
2021-04-11 15:44:47 -0400

    StatusBar and TouchableWithoutFeedback added

--- screens/PhotoUploader.js
+++ screens/PhotoUploader.js
@@ -1,7 +1,5 @@
 import React, {useState, useEffect} from 'react';
-import {View, Button, Text, TextInput, TouchableOpacity, StyleSheet} from "react-native";
-// import  * as ImagePicker from 'react-native-image-picker';
-// import {launchCamera, launchImageLibrary} from 'react-native-image-picker';
+import {View, Button, Image, Text, TextInput, StatusBar, TouchableOpacity, StyleSheet, Keyboard, TouchableWithoutFeedback} from "react-native";
 import * as ImagePicker from 'expo-image-picker';
 
 export default function PhotoUploader() {
@@ -13,70 +11,15 @@ export default function PhotoUploader() {
         setText(newText)
     }
 
-    // const selectImage = () => {
-    //     let options = {
-    //         title: 'Select Image',
-    //         maxWidth: 256,
-    //         maxHeight: 256,
-    //       storageOptions: {
-    //         skipBackup: true,
-    //         path: 'images',
-    //       },
-    //     };
-    
-        // ImagePicker.showImagePicker(options, res => {
-        //   console.log('RES==', res);
-    
-        //   if (res.didCancel) {
-        //     console.log('User cancelled photo picker');
-        //     Alert.alert('You did not select any image'); 
-        //   }     
-        //    else if (res.error) {
-        //     console.log('ImagePicker Error: ', response.error);
-        //   } 
-        //   else if (res.customButton) {
-    //         console.log('User tapped custom button: ', res.customButton);
-    //       } 
-    //       else {
-    //         let source = { uri: response.uri };
-    //     console.log({ source });
-    //       }
-    //     })       
-    // };
-    
-//   let cameraLaunch = () => {
-//     let options = {
-//       storageOptions: {
-//         skipBackup: true,
-//         path: 'images',
-//       },
-//     };
-//     console.log("Heyy")
-//     ImagePicker.launchImageLibrary(options, (res) => {
-//        console.log('Response = ', res);
-
-    //   if (res.didCancel) {
-    //     console.log('User cancelled image picker');
-    //   } else if (res.error) {
-    //     console.log('ImagePicker Error: ', res.error);
-    //   } else if (res.customButton) {
-    //     console.log('User tapped custom button: ', res.customButton);
-    //     alert(res.customButton);
-    //   } else {
-    //     const source = { uri: res.uri };
-    //     console.log('response', JSON.stringify(res));
-        
-    //   }
-//     });
-//   }
-
-
 const pickImage = async () => {
     let result = await ImagePicker.launchImageLibraryAsync({
       mediaTypes: ImagePicker.MediaTypeOptions.All,
-      allowsEditing: true,
-      aspect: [4, 3],
+      allowsEditing: false,
+    //   aspect: [4, 3], if allowsEditing: frue
       quality: 1,
+      // Specify the quality of compression,
+      // from 0 to 1. 0 means compress for small size,
+      // 1 means compress for maximum quality.
     });
 
     console.log(result);
@@ -89,13 +32,21 @@ const pickImage = async () => {
 
 
     return (
+        <>
+        <StatusBar barStyle="light-content" />
+        <TouchableWithoutFeedback onPress={()=>{Keyboard.dismiss()} }>
          <View style={styles.container}>
+             <TouchableOpacity style={styles.button}> 
+                <Button title="Pick an image" onPress={pickImage} />
+                <Image source={{ uri: image }} style={styles.image} />
+            </TouchableOpacity>
+
             <Text style = {styles.large}>Photo Description </Text>
             <Text style = {styles.small}>
                 Tell users something about this photo,
                 when/where/how it was made or story
                 what inspired you to take this shot</Text>  
-                {/* {console.log(ImagePicker)} */}
+                
             <TextInput
                 style={styles.input}
                 onChangeText={newText => changeHandler(newText)}
@@ -105,10 +56,6 @@ const pickImage = async () => {
                 required
                 multiline={true} />
 
-            <TouchableOpacity style={styles.button}> 
-                <Button title="Pick an image" onPress={pickImage} />
-                {image && <Image source={{ uri: image }} style={{ width: 200, height: 200 }} />}
-            </TouchableOpacity>
 
             <View style={styles.button}  >
                 <Button title="Submit" onPress={()=>console.log("submit")} />
@@ -116,6 +63,8 @@ const pickImage = async () => {
                             need to create custom buttom component apply styles for surounded View */}
             </View>
         </View>
+        </TouchableWithoutFeedback>
+        </>
     );
 }
 
@@ -125,7 +74,7 @@ const styles = StyleSheet.create({
     },
     input: {
         height: 70,
-        margin: 20,
+        marginTop: 10,
         padding: 10,
         borderWidth: 1,
     },
@@ -135,10 +84,17 @@ const styles = StyleSheet.create({
     },
     small: {
         fontSize: 14,
+        
     },
     button: {   
         justifyContent: "center",
         alignItems: "center",
+        margin: 10,
+    },
+    image: {
+        margin: 20,
+        width: 200,
+        height: 200 
     }
     
 })
