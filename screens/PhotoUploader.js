b35cbcf1102faf8bef0a2ff6ca3ff4447e4193d5
yulia <juliana.ny2008@gmail.com>
2021-04-11 13:20:29 -0400

    working on image-picker

--- screens/PhotoUploader.js
+++ screens/PhotoUploader.js
@@ -1,37 +1,29 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import {View, Button, Text, TextInput, TouchableOpacity, StyleSheet} from "react-native";
-import ImagePicker from 'react-native-image-picker';
+import  * as ImagePicker from 'react-native-image-picker';
+// import {launchCamera, launchImageLibrary} from 'react-native-image-picker';
 
 export default function PhotoUploader() {
 
-    const [text, setText] = React.useState("about");
+    const [text, setText] = useState("about");
 
     const changeHandler = newText => {
         setText(newText)
-      }
+    }
 
     const selectImage = () => {
         let options = {
-            title: 'Select Image',
-            // maxWidth: 256,
-            // maxHeight: 256,
-            // storageOptions: {
-            //     skipBackup: true
-            // }
-        //     customButtons: [
-        //     { 
-        //       name: 'customOptionKey', 
-        //       title: 'Choose file from Custom Option' 
-        //     },
-        //   ],
+            // title: 'Select Image',
+        //     maxWidth: 256,
+        //     maxHeight: 256,
           storageOptions: {
             skipBackup: true,
             path: 'images',
           },
         };
     
-        ImagePicker.showImagePicker(options, response => {
-          console.log('Response = ', response);
+        ImagePicker.showImagePicker(options, res => {
+          console.log('RES==', res);
     
           if (res.didCancel) {
             console.log('User cancelled photo picker');
@@ -44,13 +36,45 @@ export default function PhotoUploader() {
             console.log('User tapped custom button: ', res.customButton);
           } 
           else {
-            let source = res;
-            this.setState({
-              resourcePath: source,
-            });
+            // let source = res;
+            // this.setState({
+            //   resourcePath: source,
+            // });
+            let source = { uri: response.uri };
+        console.log({ source });
           }
         })
-    };  
+
+        
+    };
+    
+  let cameraLaunch = () => {
+    let options = {
+      storageOptions: {
+        skipBackup: true,
+        path: 'images',
+      },
+    };
+    console.log("Heyy")
+    ImagePicker.launchImageLibrary(options, (res) => {
+       console.log('Response = ', res);
+
+    //   if (res.didCancel) {
+    //     console.log('User cancelled image picker');
+    //   } else if (res.error) {
+    //     console.log('ImagePicker Error: ', res.error);
+    //   } else if (res.customButton) {
+    //     console.log('User tapped custom button: ', res.customButton);
+    //     alert(res.customButton);
+    //   } else {
+    //     const source = { uri: res.uri };
+    //     console.log('response', JSON.stringify(res));
+        
+    //   }
+    });
+  }
+
+
 
     return (
          <View style={styles.container}>
@@ -59,23 +83,25 @@ export default function PhotoUploader() {
                 Tell users something about this photo,
                 when/where/how it was made or story
                 what inspired you to take this shot</Text>  
+                {/* {console.log(ImagePicker)} */}
             <TextInput
                 style={styles.input}
                 onChangeText={newText => changeHandler(newText)}
                 value={text}
                 // placeholder="about photo"
-                keyboardType="text"
+                // keyboardType="text"
                 required
                 multiline={true} />
 
-            <TouchableOpacity onPress={selectImage}> 
-                <Text>Pick an image</Text>
+            <TouchableOpacity style={styles.button}> 
+                <Button title="Pick an image" onPress={selectImage} />
             </TouchableOpacity>
 
-            <Button title="Submit"></Button>
-                    {/* button can have color prop only, styles dont work with button component, 
-                        need to create custom buttom component*/}
-
+            <View style={styles.button}  >
+                <Button title="Submit" onPress={()=>console.log("submit")} />
+                        {/* button can have color prop only, styles dont work with button component, 
+                            need to create custom buttom component apply styles for surounded View */}
+            </View>
         </View>
     );
 }
@@ -97,5 +123,9 @@ const styles = StyleSheet.create({
     small: {
         fontSize: 14,
     },
+    button: {   
+        justifyContent: "center",
+        alignItems: "center",
+    }
     
 })
