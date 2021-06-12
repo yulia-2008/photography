9aca87c677863ebbf3cfaacd70ce311540e5db87
yulia <juliana.ny2008@gmail.com>
2021-06-12 11:49:48 -0400

    working on uploading image

--- screens/PhotoUploader.js
+++ screens/PhotoUploader.js
@@ -36,39 +36,92 @@ export default function PhotoUploader() {
                 // 1 means compress for maximum quality.
             });
 
-        if (!result.cancelled) {
+        if (!result.cancelled) { 
         setImage(result.uri)
         }
         
     }
 
     const submitHandler = () => {
-// Post to .../photos,  #create controller: creating Photo Instance and attaching an image file to the Photo instance 
         
+        let newPhotoId;
         let formData = new FormData()
-        formData.append("image", image) // creating obj with key "image" and value.
+        // formData.append("image", image) // creating obj with key "image" and value.
+        formData.append("image",  { uri: image, name: 'image.jpg', type: 'image/jpeg' })
 
-        if (image) {  console.log("img", image)
-        let options = { method: 'POST',
-                              headers: {
-                                'Content-Type': 'application/json',
-                                Accept: 'application/json'
-                              },
-                              body: JSON.stringify({
-                                 photo: {category: category,
-                                        votes: 0,
-                                        user_id: 2,
-                                        description: text,
-                                        img: formData
-                                 }
-                              })
-                            }
-        fetch('http://192.168.1.145:3000/photos', options)
-        //fetch('https://photoap-backend.herokuapp.com/photos', options)
+// 1. Creating Picture instance in DB (post to .../photos)
+// 2. Attaching an image to the Picture instance (post to .../upload_image)
+        if (image) {  
+        let createOptions = { method: 'POST',
+                        headers: {
+                            'Content-Type': 'application/json',
+                            Accept: 'application/json'
+                        },
+                        body: JSON.stringify({
+                            photo: {category: category,
+                                    votes: 0,
+                                    user_id: 2,
+                                    description: text,
+                                   }
+                        })
+                        }
+        fetch('http://192.168.1.145:3000/photos', createOptions)
         .then(response => response.json())
-        .then(resp => console.log(".../photos", resp)                    
-        )      
-        }                 
+        .then(resp => {console.log(".../photos", resp), newPhotoId=resp.photo.id, uploadImg(newPhotoId, formData)}
+        )
+     }
+     const uploadImg = (newPhotoId, formData) => {  
+         console.log(".../upload_image", newPhotoId)
+        let uploadOptions = { method: 'POST',          
+                        headers: {
+                            "Content-Type": "multipart/form-data",
+                            // make sure content-type is right
+                            Accept: 'application/json'
+                            },
+                        body:  formData
+                                            }
+        fetch(`http://192.168.1.145:3000/photos/${newPhotoId}/upload_image`, uploadOptions)
+        .then(response => response.json())
+        .then(response => console.log("../upload_image", response)
+        ) 
+                    }        
+                         
+
+//     const submitHandler = () => {
+// // Post to .../photos,  #create controller: creating Photo Instance and attaching an image file to the Photo instance 
+        
+//         let formData = new FormData()
+//         // formData.append("image", image) // creating obj with key "image" and value.
+//         // formData.append("file",  { uri: image.uri, name: 'image.jpg', type: 'image/jpeg' })
+//         let filename = image.uri.split('/').pop();
+//         formData.append("image", image.uri)
+        
+
+//         if (image) {  console.log("formData", formData)
+//         let options = { method: 'POST',
+//                         headers: {
+//                              'Content-Type': ['application/json',"multipart/form-data"],
+//                             //  "Content-Type": "multipart/form-data",
+//                              // Dont need to include 'Content-Type': 'application/json', it will cause a bug !!!!
+//                             Accept: 'application/json'
+//                         },
+//                         body: JSON.stringify({
+//                             photo: {
+//                                 category: category,
+//                                 votes: 0,
+//                                 user_id: 2,
+//                                 description: text,                          
+//                                 image:  image.uri
+//                             }                             
+//                             })
+//                         }
+
+//         fetch('http://192.168.1.145:3000/photos', options)
+//         //fetch('https://photoap-backend.herokuapp.com/photos', options)
+//         .then(response => response.json())
+//         .then(resp => console.log(".../photos", resp)                    
+//         )      
+//         }                 
     }
     
     return (
