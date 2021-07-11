8213a772bc212f57e214b7f0d684846697657575
yulia <juliana.ny2008@gmail.com>
2021-07-11 10:41:39 -0400

    removing Swipable, it is not what I needed

--- screens/PhotoDetails.js
+++ screens/PhotoDetails.js
@@ -1,7 +1,6 @@
 import React, {useState} from 'react';
 import {View, Text, Image, FlatList, TouchableOpacity, StyleSheet} from "react-native";
-// import Swiper from 'react-native-swipe-image';
-import GestureRecognizer, {swipeDirections} from 'react-native-swipe-gestures';
+
 
 
 export default function PhotoDetails({route}) {
@@ -9,58 +8,14 @@ export default function PhotoDetails({route}) {
    
     const photos = route.params.photos 
     const photoObject = route.params.photoObj
-    const [currentPhoto, setCurrentPhoto] = useState(photoObject)
-    const rightAction = () => {
-        
-        // console.log('Swipe from right');
-        setCurrentPhoto(photos[1])     
-      }
-      const leftAction = () => {
-        // console.log('Swipe from left');
-        setCurrentPhoto(photos[0]) 
-      }
-
-      const onSwipeUp = (statee) => {
-       console.log("UP", statee)
-      }
-     
-      const onSwipeDown = () => {
-        console.log("down")
-      }
-     
-      const onSwipeLeft = () => {
-       console.log("left")
-      }
-     
-      const onSwipeRight = () =>  {
-        console.log("right")
-      }
+   
     
     return (
-            <View style={styles.container}> 
-                <GestureRecognizer
-         onSwipe={console.log("onswipe")}
-        onSwipeUp={(state)=>console.log(state)}
-        onSwipeDown={()=> console.log("down)")}
-        onSwipeLeft={() => onSwipeLeft}
-        onSwipeRight={() => onSwipeRight}
-        config={{
-            velocityThreshold: 0.3,
-            directionalOffsetThreshold: 80,
-          }}
-        style={{
-          flex: 1
-        }}
-        ></GestureRecognizer>
-
-
-
-                {/* <Image style={styles.image} source={{uri: photoObject.attached_image}} />  */}
-                <Image style={styles.image} source={{uri: currentPhoto.attached_image}} /> 
-                <Text style={{textAlign: 'center'}}>Like?</Text>
-                <Text style={{textAlign: 'center'}}>Descriprion: {currentPhoto.description}</Text>  
-             </View>  
-   
+        <View style={styles.container}>
+            <Image style={styles.image} source={{uri: photoObject.attached_image}} /> 
+            <Text style={{textAlign: 'center'}}>Like?</Text>
+            <Text style={{textAlign: 'center'}}>Descriprion: {photoObject.description}</Text>   
+        </View>  
     //  In React Navigation 5.x, the navigation prop split into 2 props: navigation, route
     //  No more getParam() method, it's equivalent to: route.params          
     );
